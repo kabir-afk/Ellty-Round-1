37d2b6c8628774d3d756c70831823c03e01102e9
Kabir Singh <kabirsingh12b@gmail.com>
2025-11-22 16:04:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <>hello world</>;
+}
+
+export default App;

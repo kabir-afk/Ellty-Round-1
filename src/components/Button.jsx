f50a6884f91297a2394928d8da3553a0cb38103d
Kabir Singh <kabirsingh12b@gmail.com>
2025-11-22 16:52:42 +0530

    feat : initialized btn component

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div className="w-[340px] h-10 bg-yellow font-normal rounded-sm text-center">
+      <button type="submit" className="w-full h-10 py-2.5 px-5">
+        {" "}
+        Done
+      </button>
+    </div>
+  );
+};
+
+export default Button;
